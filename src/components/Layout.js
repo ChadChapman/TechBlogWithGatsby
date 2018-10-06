f9c53138e0bca31d0e6b5eb5618bf70f11c2c507
git_dave <ctchapman@gmail.com>
2018-10-06 14:26:28 -0700

    removed footer bar, seems redundant with the drawer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -35,7 +35,7 @@ export default class Template extends React.Component {
         </Helmet>
         <CssBaseline />
         <BlogDashboard location={this.props} />
-        <FooterBar />
+        {/*<FooterBar />*/}
 
       </React.Fragment>
     );
